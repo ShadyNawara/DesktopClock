af1b9f9adbf63a778a70aa1a58ea0e007c3fdb2b
Shady Nawara <shady.nawara@outlook.com>
2021-08-11 01:15:24 -0500

    first commit

---
+++ DesktopClock/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using WpfScreenHelper;
+
+namespace DesktopClock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// Finds main screen scalling
+        /// starts a new form in the correct position for all screens
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            object? screenScalingRegistry = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\ThemeManager", "LastLoadedDPI", "96");
+
+            float screenScalling = (screenScalingRegistry != null ? int.Parse((string)screenScalingRegistry) : 96.0f) / 96.0f;
+
+            foreach (Screen s in Screen.AllScreens)
+            {
+                if (s.Primary)
+                {
+                    continue;
+                }
+
+                Rect viewport = s.WorkingArea;
+
+                MainWindow mainWindow = new(false)
+                {
+                    Top = (viewport.Top / screenScalling) + (viewport.Height / screenScalling) - this.Height,
+                    Left = (viewport.Left / screenScalling) + (viewport.Width / screenScalling) - this.Width
+                };
+                mainWindow.Show();
+            }
+
+            Hide();
+        }
+
+        /// <summary>
+        /// secondary windows start clock tick every second
+        /// </summary>
+        /// <param name="firstWindow">whether or not this is the first window,
+        ///     not really used just to be able to overload the constructor</param>
+        public MainWindow(bool firstWindow)
+        {
+            if (!firstWindow)
+            {
+                InitializeComponent();
+                DispatcherTimer clockTicker = new();
+                clockTicker.Tick += new EventHandler(ClockTick);
+                clockTicker.Interval = new TimeSpan(0, 0, 1);
+                clockTicker.Start();
+            }
+        }
+
+        /// <summary>
+        /// Update clock text based on current time / data
+        /// adjust to format if needed
+        /// </summary>
+        public void ClockTick(object? sender, EventArgs? e)
+        {
+            timeTextBlock.Text = DateTime.Now.ToString("h:mm:ss tt");
+            dateTextBlock.Text = DateTime.Now.ToString("dddd dd, MMMM yyyy");
+        }
+
+        /// <summary>
+        /// Exit the application
+        /// </summary>
+        private void ExitButtonClicked_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-DesktopClock
+# DesktopClock
+I just upgraded to windows 11 beta and it doesnt have a clock in the task bar when using multiple monitors. This is a simple clock that shows up on the bottom right corner of all additional screens (only tested with 2 but technically should work on more)
+
+You can exit the clock from the context menu if you right click on the time/date text (make sure to point at the text not the blank space)
+
+There are no options or any interface elements other than the clock itself but I can add more options if requested
+
+## Example screenshot
+![Desktop Screenshot](./screenshot.jpg?raw=true)
+
+## Binary Releases
+There are 2 pre-built binaries available
+
+- A self contained clickonce setup (250mb)
+- An executable only version that requires .net 6 (87kb)
